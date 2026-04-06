4a3e96fb75d7aade703de26c7ba31e8312dfe34d
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2026-04-06 23:32:25 +0530

    Update MainLayout.jsx

--- OIMS Frontend/src/layouts/MainLayout.jsx
+++ OIMS Frontend/src/layouts/MainLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import { Box, Toolbar } from "@mui/material";
 
 const MainLayout = () => {
   return (
-    <div className=" w-full  ">
-      {/* <Header /> */}
-
-      <Outlet />
-      {/* <Footer />
-      <MobileBottomMenu/> */}
-    </div>
+    <Box sx={{ display: 'flex' }} className="min-h-screen bg-slate-50 w-full">
+      <Sidebar />
+      <Box component="main" sx={{ flexGrow: 1, p: 3, pt: 8 }}>
+        <Outlet />
+      </Box>
+    </Box>
   );
 };
 
